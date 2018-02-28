9c6d84cc21b37384f7652c8cf83133fec339c5ed
Vishal Deep <visdhiman15@gmail.com>
2018-02-27 19:26:48 -0500

    added styling to show page

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 
 // renders landing page
